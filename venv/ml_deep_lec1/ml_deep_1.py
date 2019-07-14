b2c231633eaed86d566fb7898369ec3dc097693a
darktaoist <darktaoist@gmail.com>
2019-07-14 14:27:43 +0900

    머신러닝 딥러닝 1강f

--- venv/ml_deep_lec1/ml_deep_1.py
+++ venv/ml_deep_lec1/ml_deep_1.py
@@ -1,3 +1,13 @@
 """
-입력    출력  력  력
+
+지도(교사)학습(Supervised Learning)
+  - 학습 데이터마다 레이블을 가지고 있음
+
+비지도학습(Unsupervised Learning)
+    - 학습 데이터가 레이블을 가지고 있지 않음
+
+준지도학습(Semi_Supervised Learning)
+
+강화학습(Reinforcement Learning)
+
 """
