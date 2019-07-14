5499df6d50404e2b4a47bd537ac29a28596ad00c
darktaoist <darktaoist@gmail.com>
2019-07-14 14:44:42 +0900

    머신러닝 딥러닝 1강f

--- venv/ml_deep_lec1/ml_deep_1.py
+++ venv/ml_deep_lec1/ml_deep_1.py
@@ -1,13 +1,27 @@
 """
+머신러닝 알고리즘 분류
 
-지도(교사)학습(Supervised Learning)
-  - 학습 데이터마다 레이블을 가지고 있음
+1. 지도(교사)학습(Supervised Learning)
+    - 학습 데이터마다 레이블을 가지고 있음
+    - x, y  F(x) = y
+    - x는 입력 데이타
+    - y는 레이블(결과값)
+    - D = {x,y}
 
-비지도학습(Unsupervised Learning)
+2. 비지도학습(Unsupervised Learning)
     - 학습 데이터가 레이블을 가지고 있지 않음
+    - x가 있으나 y가 없음
+    - D = {x}
+    - F(x) = X(x의 고유한 특징)
+    - Clustering(군집화)
 
-준지도학습(Semi_Supervised Learning)
+3. 준지도학습(Semi_Supervised Learning)
+    - 지도학습과 비지도 학습의 중간
+    - 학습 데이터가 약간의 레이블을 가지고 있음
 
-강화학습(Reinforcement Learning)
+4. 강화학습(Reinforcement Learning)
+    - 최종 출력이 바로 주어지지 않고 시간이 지나서 주어지는 경우
+    - 바둑
+
 
 """
