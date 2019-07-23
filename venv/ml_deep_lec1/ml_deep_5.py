4f4bfcfa4e1bee9419400a7ba630e4f47014fffc
shinhan.kim <darktaoist@gmail.com>
2019-07-24 08:03:11 +0900

    SVC 예제

--- venv/ml_deep_lec1/ml_deep_5.py
+++ venv/ml_deep_lec1/ml_deep_5.py
@@ -7,7 +7,8 @@ xor_data = [
     [1,1,0]
 ]
 
-data = []
+#주어진 데이터를 분리한다 (학습데이터와 레이블을 분리한다)
+training_data = []
 label = []
 
 for row in xor_data:
@@ -15,11 +16,26 @@ for row in xor_data:
     q = row[1]
     res = row[2]
 
-    data.append([p,q])
+    training_data.append([p,q])
     label.append(res)
 
 #SVM알고리즘을 사용하는 머신러닝 객체를 만든다.
 clf = svm.SVC()
 
 #fit() 메소드 학습기계에 데애터를 학습시킨다.
+clf.fit(training_data, label)
+
+#predict()메소드 : 학습데이터를 이용하여 예측한다.
+pre = clf.predict(training_data)
+
+print(pre)
+
+ok = 0; total =0
+
+for idx, answer in enumerate(label):
+    p = pre[idx]
+    if p == answer: ok += 1
+    total += 1
+
+print("ok:" , ok ,"total :" , total , "accuracy : ", (ok/total)*100,"%")
 
